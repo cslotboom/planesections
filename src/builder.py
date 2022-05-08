f14bd5b3bb444f8396473ed6dff3e0e52c8da779
cslotboom <50510141+cslotboom@users.noreply.github.com>
2022-05-07 22:25:02 -0700

    Major updates to the base beam class
    Updating the base beam class so that nodes no longer contain point loads.
    Updating node ID so that it starts at one. The index will always be one less than the ID.

--- src/builder.py
+++ src/builder.py
@@ -22,97 +22,229 @@ import matplotlib.pyplot as plt
 # Summarize Nodes?
 # Summarize Loads?
 
-class beamBuilder():
+
+class Section():
+    pass
+
+
+
+
+class Node2D():
        
+    def __init__(self, x, fixity, label = None):
+        """
+        Represents a node.
+        Nodes have labels and IDs. 
+        The Labe is a name the user assigns to the node and will be displayed
+        in plots.
+        
+        The ID is a unique name that OpenSees will read. ID - 1 will be the 
+        position in the beam node array
+
+        Parameters
+        ----------
+        x : float
+            The postion of the node.
+        fixity : list
+            A list of the input fixities for nodes.
+        pointLoad : PointLoad
+            DESCRIPTION.
+        label : TYPE, optional
+            DESCRIPTION. The default is None.
+
+        """
+        
+        
+        self.x = x
+
+        self.fixity = fixity
+        self.ID = None
+        
+        self.label = label
+        
+        self.pointLoadIDs = []
+        
+        self.disp = None
+        self.rFrc = None
+        self.Fint = None
+
+        self.hasReaction = False
+        if np.any(fixity != [0,0,0]):
+            self.hasReaction = True
+
+    def setID(self, newID):
+        self.ID  = newID
+        
+    def __repr__(self):
+        return f'Node object at {self.x}'
+
+class Beam2D():
+    """
+    A representation of a beam object, that can be used to define information
+    about basic beams.
+    """
+
+    def _initArrays(self):
+        self.nodeLabels = {}
+        self.nodes = []
+        self.pointLoads = []
+        self.distLoads = []    
+    
     def sortNodes(self):
         """
         Sorts and renames the nodes based on their x coordinate.
-        """        
-        
+        This is expensive. Try to minimize calls to it.
+        """               
         xcoords = np.zeros(self.Nnodes)
+        labels  = [None] * self.Nnodes
         for ii, node in enumerate(self.nodes):
             xcoords[ii] = node.x
-            
-        sortedInd = np.argsort(xcoords)
+            labels[ii]  = node.label
+        
+        # subtract 1 because the indexes are one less.
+        oldInd = np.array(self.getNodeIDs())
+        # print(oldInd)
+        
+        # Sort the nodes.
+        # print(xcoords)
+        sortedInd   = np.argsort(xcoords)
         sortedNodes = np.array(self.nodes)[sortedInd]
+        self.nodes  = list(sortedNodes)
         
-        self.nodes = list(sortedNodes)
+        # print(sortedInd)
+        self.resetNodeID()
+        self.remapLabels(sortedInd)
+        self.remapPointLoads(oldInd, sortedInd)    
         
-        self.relabelNodes()
+    def getNodeIDs(self):
         
-    def relabelNodes(self):
+        IDs = [None]*self.Nnodes
+        for ii, node in enumerate(self.nodes):
+            IDs[ii] = node.ID
+            # print(node.ID)
+        
+        return IDs
+           
+    def resetNodeID(self):
         """
-        renames all the nodes based on their position in the list.
+        Resets all the node IDs based on their position. The ID starts counting
+        at the left-most label
         """
         for ii, node in enumerate(self.nodes):
-            node.ID = int(ii + 1)
+            node.setID(int(ii + 1))
+
+    def remapPointLoads(self, oldIDs, sortedInd):
+        """
+        We don't know where the position is because of the newly added node'
+        """        
+        sortedIDs = np.array(oldIDs)[sortedInd]
+        # print(sortedIDs)
+        for pointLoad in self.pointLoads:
+            newInd = np.where( sortedIDs == pointLoad.nodeID)[0][0]
+            pointLoad.nodeID = newInd
+        
+    def remapLabels(self, sortedInd):
+        oldLabels = self.nodeLabels
+        newLabel = {}
+        for label in oldLabels:
+            newLabel[label] = sortedInd[oldLabels[label]]
+        self.nodeLabels = newLabel   
     
     def _addedNodeMessage(self, x):
         print(f'New node added at: {x}')
     
-    def addNode(self, x, fixity = np.array([0.,0.,0.]), pointLoad = np.array([0.,0.,0.]), ID = None):
+    def addNode(self, x, fixity = np.array([0.,0.,0.], int), label = None, sort = True):
         """
         Adds a new node to the model builder.
 
         Parameters
         ----------
-        x : flaot
+        x : float
             The x coordinate of the node.
         fixity : np.array
             The fixity array. Contains 3 values, one for each dof in order
-            x, y, rotation. 1 means the system is fixed at said node, 
-            0 means their is no fixity conditon.
+            x, y, rotation. 1 means the system is fixed in the DOF of question, 
+            0 means the node is free in the DOF.
         pointLoad : np.array
             The array of loads applied ot the system. Contains 3 values, 
-            ne for each dof in order Px, Py, Moment.
-        ID : int, optional
-            The ID of the node in question. Nodes are ordered by position,
-            starting at the left most node and ending with the right most node.
-            1 --- 2 ---3 - 4 ------- ... -- N
+            one for each dof in order Px, Py, Moment.
+        label : int, optional
+            The unique label of the node in question. 
 
         Returns
         -------
-        None.
+        flag: int
+            returns 0 if a existing node has been updated, 1 if a new node is
+            added, and -1 if the process failed.
 
         """
+        newNode = Node2D(x, fixity, label)
+        # newNode.pointLoadIDs.append(pointLoadID)
+        if x in self.nodeCoords:
+            index = self._findNode(x)
+            self.nodes[index] = newNode
+            
+            return 0
+        else:
+            # print(newNode)
+            self._addNewNode(newNode, sort)
+            return 1
+        return -1
 
+
+    def _addNewNode(self, newNode:Node2D, sort=True):
         self.Nnodes += 1
-        
-        if ID == None:
-            ID = self.Nnodes
-        
-        newNode = Node(x, fixity, pointLoad, ID)
+        newNode.setID(self.Nnodes)
         self.nodes.append(newNode)
-        self.nodeCoords.add(x)
+        self.nodeCoords.add(newNode.x)
+        if sort:
+            self.sortNodes()
         
-        self.sortNodes()
+
+    def addNodes(self, xCoords, fixities = None, labels = None ):
+        """
+        Adds several new nodes to the beam at the same time.
+        The nodes in question are added at the x coordinates in the model.
+        Nodes are sorted at the end of the process
         
-    def addNodes(self, xCoords, fixities = None, pointLoads = None ):
-        
-        
-        
+        Parameters
+        ----------
+        xCoords : list of float
+            A list of the x coordinates to be added to the model.
+        fixities : list of lists, optional
+            Adds fixity to each of the new nodes. Fixity is given in a list 
+            of boolean, where true means that the node if fixed in that DOF.
+            The default is None, which results in all nodes being free.
+            Ex.
+            [[1,1,0], [0,0,0]]
+            New node 1: fixed in x, fixed in y, free in rotation.
+            New node 2: free in all DOF.
+        pointLoads : list of lists, optional
+            Adds fixity to each of the new nodes. Fixity is given in a list 
+            of boolean, where true means that the node if fixed in that DOF.
+            The default is None, which results in all nodes being free.
+            Ex.
+           [[0,10.,0], [0,0,13]]
+           New node 1: A vertical load of 10 is applied in beam units.
+           New node 2: A moment of 13 is applied in beam units.
+
+        """
+               
         newNoads = len(xCoords)
-        self.Nnodes += newNoads
+        # self.Nnodes += newNoads
         
         if fixities == None:
             fixities = [np.array([0.,0.,0.])]*newNoads
+                        
+        if labels is None:
+            labels = [None]*newNoads
             
-        if pointLoads is None:
-            pointLoads = [np.array([0.,0.,0.])]*newNoads
-            
-        # for ii in range(newNoads):
-        #     self.addNode(xCoords[ii], fixities[ii], pointLoads[ii])    
+        sort = False #only sort at the end!
         for ii in range(newNoads):
-            newNode = Node(xCoords[ii], fixities[ii], pointLoads[ii], ii)
-            self.nodes.append(newNode)
-            self.nodeCoords.add(xCoords[ii])
-            # self.addNode(xCoords[ii], fixities[ii], pointLoads[ii])        
-    
-    
-    
+            self.addNode(xCoords[ii], fixities[ii], labels[ii], sort)     
+
         self.sortNodes()
-    
-    
+        
     def _checkfixityInput(self, fixity):
         
         """
@@ -125,7 +257,6 @@ class beamBuilder():
         if len(fixity) == 2 or len(fixity) > 3:
             raise ValueError("Fixity must be a integer or vector of length 3")
        
-        
     def _convertFixityInput(self, fixity):
         """
         If an integer is supplied, convert the input to a list.
@@ -165,7 +296,8 @@ class beamBuilder():
     def addPointLoad(self, x, pointLoad):
         """
         Adds a load ot the model at location x.
-        Old loads are deleted.
+        If a node exists at the current location, the old load value is overwritten.
+        Old loads are deleted, and the node is relabled
         
         Parameters
         ----------
@@ -175,31 +307,49 @@ class beamBuilder():
             A list of the forces in [Fx, Fy, M].
 
         """
-        # Check if the node is in the list of coordinates used
+        
+        loadID = len(self.pointLoads) + 1
+        
+        # Add the load ID to the node in question. Make a node if no ID.
         if x in self.nodeCoords:
-            index = self._findNode(x)
-            self.nodes[index].pointLoad = pointLoad
+            nodeIndex = self._findNode(x)
         else:
-            fixity = np.array([0,0,0], int)
-            self.addNode(x, fixity, pointLoad)
-     
-    def addPointLoads(self, x, pointLoad):
-        pass
-
-    
+            self.addNode(x)
+            nodeIndex = self._findNode(x)
+        nodeID = nodeIndex + 1
+        # print(nodeID)
+            
+        self.nodes[nodeIndex].pointLoadIDs.append(loadID) 
+        newLoad = PointLoad(pointLoad, nodeID)
+        self.pointLoads.append(newLoad)                
+         
     def addVerticalLoad(self, x, Py):
         """
         Adds a vertical load to the model at location x.
-        Old loads are deleted.
-        """        
+        Old loads at this point are deleted.
+        
+        Parameters
+        ----------
+        x : float
+            The x location to add a moment at.
+        M : float
+            The magnitude of the moment to be added at x.
+        """   
 
         pointLoad = np.array([0., Py, 0.])
         self.addPointLoad(x, pointLoad)
         
     def addMoment(self, x, M):
         """
-        Adds a Moment ot the model at location x.
-        Old loads are deleted.
+        Adds a moment ot the model at location x.
+        Old loads at this point are deleted.
+        
+        Parameters
+        ----------
+        x : float
+            The x location to add a moment at.
+        M : float
+            The magnitude of the moment to be added at x.
         """        
         pointLoad = np.array([0.,0., M])
         self.addPointLoad(x, pointLoad)     
@@ -268,17 +418,80 @@ class beamBuilder():
         newEleLoad = EleLoad(x1, x2, distLoad)
         self.eleLoads.append(newEleLoad)
 
+    
     def plot(self):
         xcoords = np.array(list(self.nodeCoords))
         y = np.zeros_like(xcoords)
         plt.plot(xcoords, y)
         plt.plot(xcoords, y, '.')
 
+    """OuputPropreties"""
+    def Fmax(self, index):
+        Fmax = 0
+        Fmin = 0
+        for node in self.nodes:
+            F1 = node.Fint[index]
+            F2 = node.Fint[index + 3]
+            
+            if F1 < Fmin or F2 < Fmin:
+                Fmin = min(F1, F2)
+            
+            if Fmax < F1 or Fmax < F2:
+                Fmax = max(F1, F2)
+        return Fmin, Fmax
 
+
+    def getNodes(self):
+        return self.nodes
+    
+    
+    def getForces():
+        pass
+    
+    def getDistForses():
+        pass 
+
+# =============================================================================
+# 
+# =============================================================================
+
+class EulerBeam2D(Beam2D):
+
+    def __init__(self, xcoords = [], fixities = [], labels = [], 
+                 E = 1., A=1., I=1., geomTransform = 'Linear'):
+        
+        # print(self.nodeLabels)
+        #
+        self._initArrays()
+        # geomTransform has values 'Linear' or 'PDelta'
+        self.nodes = []
+        self.eleLoads = []
+        self.nodeCoords = set()
+        self.materialPropreties = [E, A, I]  
+            
+        self.Nnodes = 0
+        NnewNodes = len(xcoords)
+       
+        if len(fixities) == 0:
+            fixities = [np.array([0., 0., 0.])] * NnewNodes
+            
+        if len(labels) == 0:
+            labels = [None] * NnewNodes            
+        if len(xcoords) != 0:
+            self.addNodes(xcoords, fixities, labels)
+        
+        # pointLoad = np.array([0., 0., 0.])
+        # for ii in range(NnewLoad):
+        #     self.addNode(xcoords[ii], fixities[ii], pointLoad)
+        
+        self.plotter = None
+        
+        self.geomTransform = geomTransform
+        self.EleType = 'elasticBeamColumn'
+    
     @property
     def Mmax(self):
         return self.Fmax(2)
-
 
     @property
     def reactions(self):
@@ -303,77 +516,6 @@ class beamBuilder():
         return self.Fmax(1)
 
 
-    def Fmax(self, index):
-        Fmax = 0
-        Fmin = 0
-        for node in self.nodes:
-            F1 = node.Fint[index]
-            F2 = node.Fint[index + 3]
-            
-            if F1 < Fmin or F2 < Fmin:
-                Fmin = min(F1, F2)
-            
-            if Fmax < F1 or Fmax < F2:
-                Fmax = max(F1, F2)
-        return Fmin, Fmax
-
-# =============================================================================
-# 
-# =============================================================================
-
-
-#TODO: make intiail mesh
-class EulerBeam(beamBuilder):
-
-    def __init__(self, xcoords = [], fixities = [], E = 1., A=1., I=1., geomTransform = 'Linear'):
-        
-        # geomTransform has values 'Linear' or 'PDelta'
-        self.nodes = []
-        self.eleLoads = []
-        self.nodeCoords = set()
-        self.materialPropreties = [E, A, I]  
-            
-        self.Nnodes = 0
-        NnewLoad = len(xcoords)
-        pointLoad = [np.array([0., 0., 0.])] * NnewLoad
-        
-        
-        if len(fixities) == 0:
-            fixities = [np.array([0., 0., 0.])] * NnewLoad
-        self.addNodes(xcoords, fixities, pointLoad)
-        
-        pointLoad = np.array([0., 0., 0.])
-        # for ii in range(NnewLoad):
-        #     self.addNode(xcoords[ii], fixities[ii], pointLoad)
-        
-        self.plotter = None
-        
-        self.geomTransform = geomTransform
-        self.EleType = 'elasticBeamColumn'
-    
-    # def setReactionNodes:
-        
-    #     self.reactionNodes = []
-    #     for node in self.nodes:
-    #         if node.fixity !=
-
-
-class Node():
-       
-    def __init__(self, x, fixity, pointLoad, ID):
-        self.x = x
-        self.pointLoad = pointLoad
-        self.fixity = fixity
-        self.ID = ID
-        
-        self.disp = None
-        self.rFrc = None
-        self.Fint = None
-        
-        self.hasReaction = False
-        if np.any(fixity != [0,0,0]):
-            self.hasReaction = True
-
 
 # class Element():
        
@@ -384,7 +526,7 @@ class Node():
 
 
 
-class EleLoad():
+class EleLoad:
     def __init__(self, x1, x2, distLoad):
         self.x1 = x1
         self.x2 = x2
@@ -393,12 +535,19 @@ class EleLoad():
 
 
 
-class PointLoad():
+class PointLoad:
     P = np.array([0.,0.,0.])
-    x = 0.
+    nodeID = None
     
-    def __init__(self, P):
-        pass
+    def __init__(self, P, nodeID):
+        self.P = P
+        self.nodeID = nodeID
+        
+    def setID(self, newID):
+        # print(newID)
+        self.nodeID = newID
+        
+        
 
 class DistLoad():
     
