afb9ef8fbc66a5e1c155b4238a7f27dfed55517f
cslotboom <50510141+cslotboom@users.noreply.github.com>
2022-07-01 23:34:02 -0700

    Trying to fix build path issues for readthedocs

--- doc/readthedocs/source/conf.py
+++ doc/readthedocs/source/conf.py
@@ -14,6 +14,9 @@ import os
 import sys
 #sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, os.path.abspath('../source'))
+sys.path.insert(0, os.path.abspath("."))
+sys.path.insert(0, os.path.abspath("../"))
+
 
 import sphinx_rtd_theme
 # -- Project information -----------------------------------------------------
