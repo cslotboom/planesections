5666c8c62ecaa15ade4a6f7f4c186d50b3f7e364
cslotboom <50510141+cslotboom@users.noreply.github.com>
2022-07-01 23:38:16 -0700

    Update conf.py

--- doc/readthedocs/source/conf.py
+++ doc/readthedocs/source/conf.py
@@ -15,7 +15,7 @@ import sys
 #sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, os.path.abspath('../source'))
 sys.path.insert(0, os.path.abspath("."))
-sys.path.insert(0, os.path.abspath("../"))
+sys.path.insert(0, os.path.abspath(".."))
 
 
 import sphinx_rtd_theme
