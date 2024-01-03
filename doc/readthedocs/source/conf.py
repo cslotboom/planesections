f19436a47330e50dbfc77cefc971dd42856fe453
cslotboom <50510141+cslotboom@users.noreply.github.com>
2024-01-02 21:47:42 -0800

    fix readthedocs

--- doc/readthedocs/source/conf.py
+++ doc/readthedocs/source/conf.py
@@ -26,7 +26,7 @@ copyright = '2020, CSlotboom'
 author = 'CSlotboom'
 
 # The full version, including alpha/beta/rc tags
-release = '1.2.0'
+release = '1.2.2'
 
 
 # -- General configuration ---------------------------------------------------
